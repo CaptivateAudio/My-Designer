50592109b3b14d4d9092f51543498b28d6058ea2
Lester Joson <lester@podcastwebsites.com>
2018-10-26 16:31:05 +0800

    Add users view - list all users - edit user - create user

---
+++ mydesigner/resources/views/admin/users/create.blade.php
@@ -0,0 +1,88 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading"><h1>Add New User</h1></div>
+
+                @if(session()->has('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                @endif
+
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
+                <div class="panel-body">
+                    <form action="{{ route('admin.users.store') }}" method="post">
+                        {{ csrf_field() }}
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">First Name</label>
+                            <div class="col-sm-8">
+                                <input type="text" name="first_name" class="form-control" value="" />
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Last Name</label>
+                            <div class="col-sm-8">
+                                <input type="text" name="last_name" class="form-control" value="" />
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Role</label>
+                            <div class="col-sm-8">
+                                <select name="role" class="form-control">
+                                    @foreach ( $roles as $role )
+                                        <option value="{{ $role->id }}">{{ $role->role_name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Email</label>
+                            <div class="col-sm-8">
+                                <input type="email" name="email" class="form-control" value="" />
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Password</label>
+                            <div class="col-sm-8">
+                                <input type="password" name="password" class="form-control" />
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Credits</label>
+                            <div class="col-sm-8">
+                                <input type="number" name="credits" class="form-control" value="0" />
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label"></label>
+                            <div class="col-sm-8">
+                                <button class="btn btn-primary" type="submit">Create</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ mydesigner/resources/views/admin/users/index.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+
+                <div class="row">
+                    <div class="col-md-8"><div class="panel-heading"><h1>Users</h1></div></div>
+                    <div class="col-md-4"><p class="text-right"><a href="{{ route('admin.users.create') }}" class="btn btn-primary">Add New User</a></p></div>
+                </div>
+
+                <div class="panel-body">
+
+                    @if(session()->has('success'))
+                        <div class="alert alert-success">
+                            {{ session('success') }}
+                        </div>
+                    @endif
+                    
+                    <table id="usersTable" class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Role</th>
+                                <th>Email</th>
+                                <th>Credits</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse($users as $user)
+                            <tr>
+                                <td>{{ $user->id }}</td>
+                                <td>{{ $user->first_name }} {{ $user->last_name }}</td>
+                                <td>{{ $user->roles()->where('user_id', $user->id)->first()->role_name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>{{ $user->credits }}</td>
+                                <td>
+                                    <a href="{{ route('admin.users.edit', $user->id) }}" class="btn btn-outline-primary">Edit</a>
+                                    <form action="{{ route('admin.users.destroy', $user->id) }}" method="POST"
+                                          style="display: inline"
+                                          onsubmit="return confirm('Are you sure?');">
+                                        <input type="hidden" name="_method" value="DELETE">
+                                        {{ csrf_field() }}
+                                        <button class="btn btn-outline-danger">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="3">No entries found.</td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript">
+jQuery(document).ready( function () {
+    jQuery('#usersTable').DataTable();
+} );
+</script>
+
+@endsection
