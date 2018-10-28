e91572c10a877741d081bc4518c1cf14ad71f3c4
Lester Joson <lester@podcastwebsites.com>
2018-10-28 21:29:52 +0800

    Add migrations - Teams table - Team_user table - Packages table - Package_team table

---
+++ mydesigner/database/migrations/2018_10_27_113314_create_package_team_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePackageTeamTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('package_team', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('package_id')->unsigned();
+            $table->integer('team_id')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('package_team');
+    }
+}
