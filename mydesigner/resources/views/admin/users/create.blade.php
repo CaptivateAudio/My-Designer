278b3560b218f4fc866dd5764fb2c307e238e2d1
Lester Joson <lester@podcastwebsites.com>
2018-11-09 23:21:58 +0800

    Update admin users create view

--- mydesigner/resources/views/admin/users/create.blade.php
+++ mydesigner/resources/views/admin/users/create.blade.php
@@ -1,89 +1,105 @@
-@extends('layouts.app')
+@extends('layouts.admin')
+
+@section('title', 'Users')
+
+@section('heading')
+<div id="admin-content-heading">
+    <div class="container-fluid">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <h2>Add New User</h2>
+            </div>
+            <div class="col-md-6 text-right">Admin > Users > Add New</div>
+        </div>
+    </div>
+</div>
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading"><h1>Add New User</h1></div>
+<div id="admin-content-wrap">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
 
-                @if(session()->has('success'))
-                    <div class="alert alert-success">
-                        {{ session('success') }}
+                    @if(session()->has('success'))
+                        <div class="alert alert-success">
+                            {{ session('success') }}
+                        </div>
+                    @endif
+
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    <div class="panel-body">
+                        <form action="{{ route('admin.users.store') }}" method="post">
+                            {{ csrf_field() }}
+
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">First Name</label>
+                                <div class="col-sm-9">
+                                    <input type="text" name="first_name" class="form-control" value="" />
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Last Name</label>
+                                <div class="col-sm-9">
+                                    <input type="text" name="last_name" class="form-control" value="" />
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Role</label>
+                                <div class="col-sm-9">
+                                    <select name="role" class="form-control text-capitalize">
+                                        @foreach ( $roles as $role )
+                                            <option value="{{ $role->id }}" {{ ( $role->role_name == 'user' ) ? 'selected' : '' }}>{{ $role->role_name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Team</label>
+                                <div class="col-sm-9">
+                                    <select name="team" class="form-control">
+                                        @foreach ( $teams as $team )
+                                            <option value="{{ $team->id }}">{{ $team->team_name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Email</label>
+                                <div class="col-sm-9">
+                                    <input type="email" name="email" class="form-control" value="" />
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Password</label>
+                                <div class="col-sm-9">
+                                    <input type="password" name="password" class="form-control" />
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label"></label>
+                                <div class="col-sm-9">
+                                    <button class="btn btn-primary" type="submit">Submit</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                @endif
-
-                @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
-
-                <div class="panel-body">
-                    <form action="{{ route('admin.users.store') }}" method="post">
-                        {{ csrf_field() }}
-
-                        <div class="form-group row">
-                            <label class="col-sm-4 col-form-label">First Name</label>
-                            <div class="col-sm-8">
-                                <input type="text" name="first_name" class="form-control" value="" />
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label class="col-sm-4 col-form-label">Last Name</label>
-                            <div class="col-sm-8">
-                                <input type="text" name="last_name" class="form-control" value="" />
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label class="col-sm-4 col-form-label">Role</label>
-                            <div class="col-sm-8">
-                                <select name="role" class="form-control text-capitalize">
-                                    @foreach ( $roles as $role )
-                                        <option value="{{ $role->id }}" {{ ( $role->role_name == 'user' ) ? 'selected' : '' }}>{{ $role->role_name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label class="col-sm-4 col-form-label">Team</label>
-                            <div class="col-sm-8">
-                                <select name="team" class="form-control">
-                                    @foreach ( $teams as $team )
-                                        <option value="{{ $team->id }}">{{ $team->team_name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label class="col-sm-4 col-form-label">Email</label>
-                            <div class="col-sm-8">
-                                <input type="email" name="email" class="form-control" value="" />
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label class="col-sm-4 col-form-label">Password</label>
-                            <div class="col-sm-8">
-                                <input type="password" name="password" class="form-control" />
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label class="col-sm-4 col-form-label"></label>
-                            <div class="col-sm-8">
-                                <button class="btn btn-primary" type="submit">Create</button>
-                            </div>
-                        </div>
-                    </form>
                 </div>
             </div>
         </div>
