364702e057040372a2bf98c08ef679290061f323
Lester Joson <lester@podcastwebsites.com>
2018-11-09 23:19:14 +0800

    Add Design model

---
+++ mydesigner/app/Models/Design.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace MyDesigner\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Design extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'status', 'details', 'completion_date', 'package_id',
+    ];
+
+	public function users()
+	{
+	  return $this->belongsToMany(User::class);
+	}
+
+	public function feedback()
+    {
+        return $this->hasMany(Feedback::class);
+    }
+
+    public function package()
+    {
+        return $this->belongsTo(Package::class);
+    }
+}
