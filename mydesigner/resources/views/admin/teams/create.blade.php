bcc4f54ea0f4eb5c74d6eb947bad234680ffa7b4
Lester Joson <lester@podcastwebsites.com>
2018-11-09 23:22:33 +0800

    Update admin teams create view

--- mydesigner/resources/views/admin/teams/create.blade.php
+++ mydesigner/resources/views/admin/teams/create.blade.php
@@ -1,46 +1,62 @@
-@extends('layouts.app')
+@extends('layouts.admin')
+
+@section('title', 'Teams')
+
+@section('heading')
+<div id="admin-content-heading">
+    <div class="container-fluid">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <h2>Add New Team</h2>
+            </div>
+            <div class="col-md-6 text-right">Admin > Teams > Add New</div>
+        </div>
+    </div>
+</div>
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading"><h1>Add New Team</h1></div>
+<div id="admin-content-wrap">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
 
-                @if(session()->has('success'))
-                    <div class="alert alert-success">
-                        {{ session('success') }}
-                    </div>
-                @endif
-
-                @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
-
-                <div class="panel-body">
-                    <form action="{{ route('admin.teams.store') }}" method="post">
-                        {{ csrf_field() }}
-
-                        <div class="form-group row">
-                            <label class="col-sm-4 col-form-label">Team Name</label>
-                            <div class="col-sm-8">
-                                <input type="text" name="team_name" class="form-control" value="" />
-                            </div>
+                    @if(session()->has('success'))
+                        <div class="alert alert-success">
+                            {{ session('success') }}
                         </div>
+                    @endif
 
-                        <div class="form-group row">
-                            <label class="col-sm-4 col-form-label"></label>
-                            <div class="col-sm-8">
-                                <button class="btn btn-primary" type="submit">Create</button>
-                            </div>
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
                         </div>
-                    </form>
+                    @endif
+
+                    <div class="panel-body">
+                        <form action="{{ route('admin.teams.store') }}" method="post">
+                            {{ csrf_field() }}
+
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Team Name</label>
+                                <div class="col-sm-9">
+                                    <input type="text" name="team_name" class="form-control" value="" />
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label"></label>
+                                <div class="col-sm-9">
+                                    <button class="btn btn-primary" type="submit">Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
