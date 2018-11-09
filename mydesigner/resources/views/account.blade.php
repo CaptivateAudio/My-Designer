d214b3aa5e52a46e5153a5323d170d76e6fefbde
Lester Joson <lester@podcastwebsites.com>
2018-11-09 23:24:22 +0800

    Update account view

--- mydesigner/resources/views/account.blade.php
+++ mydesigner/resources/views/account.blade.php
@@ -1,34 +1,51 @@
-@extends('layouts.app')
+@extends('layouts.admin')
+
+@section('title', 'Account')
+
+@section('heading')
+<div id="admin-content-heading">
+    <div class="container-fluid">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <h2>Account</h2>
+            </div>
+            <div class="col-md-6 text-right"><span class="text-capitalize">{{ Auth::user()->roles->first()->role_name }}</span> > Account</div>
+        </div>
+    </div>
+</div>
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Account</div>
+<div id="admin-content-wrap">
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Account</div>
 
-                <div class="card-body">
-					<p>Email: {{ $user->email }}</p>
+                    <div class="card-body">
+    					<p>Email: {{ $user->email }}</p>
+                    </div>
+    				
                 </div>
-				
-            </div>
 
-            <p>&nbsp;</p>
+                <p>&nbsp;</p>
 
-            <div class="card">
-                <div class="card-header">Profile</div>
+                <div class="card">
+                    <div class="card-header">Profile</div>
 
-                <div class="card-body">
-                    <p>First Name: {{ $user->first_name }}</p>
-                    <p>Last Name: {{ $user->last_name }}</p>
+                    <div class="card-body">
+                        <p>First Name: {{ $user->first_name }}</p>
+                        <p>Last Name: {{ $user->last_name }}</p>
+                    </div>
+                    
                 </div>
-                
+
+                <p>&nbsp;</p>
+
+
+                <p><a class="btn btn-primary" href="{{ url('/edit-account') }}">{{ __('Edit') }}</a></p>
             </div>
-
-            <p>&nbsp;</p>
-
-
-            <p><a class="btn btn-primary" href="{{ url('/edit-account') }}">{{ __('Edit') }}</a></p>
         </div>
     </div>
 </div>
