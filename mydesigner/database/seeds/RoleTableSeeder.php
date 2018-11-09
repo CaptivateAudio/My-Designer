1616be74f09497122ea0d0ff1e6c55a9488704a1
Lester Joson <lester@podcastwebsites.com>
2018-11-09 23:25:00 +0800

    Update RoleTableSeeder - add manager role

--- mydesigner/database/seeds/RoleTableSeeder.php
+++ mydesigner/database/seeds/RoleTableSeeder.php
@@ -12,16 +12,20 @@ class RoleTableSeeder extends Seeder
      */
     public function run()
     {
-        $role_employee = new Role();
-		$role_employee->role_name = 'admin';
-		$role_employee->save();
+        $role_admin = new Role();
+		$role_admin->role_name = 'admin';
+		$role_admin->save();
 
-		$role_manager = new Role();
-		$role_manager->role_name = 'user';
-		$role_manager->save();
+		$role_user = new Role();
+		$role_user->role_name = 'user';
+		$role_user->save();
 
+		$role_designer = new Role();
+		$role_designer->role_name = 'designer';
+		$role_designer->save();
+		
 		$role_manager = new Role();
-		$role_manager->role_name = 'designer';
+		$role_manager->role_name = 'manager';
 		$role_manager->save();
     }
 }
