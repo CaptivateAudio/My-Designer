b616d59126b830cce84c4a2523ee79bc76a2bc78
Lester Joson <lester@podcastwebsites.com>
2018-11-09 23:18:46 +0800

    Create designs table

---
+++ mydesigner/database/migrations/2018_11_07_094935_create_designs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDesignsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('designs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('status')->default('pending');
+            $table->text('details')->nullable();
+            $table->date('completion_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('designs');
+    }
+}
