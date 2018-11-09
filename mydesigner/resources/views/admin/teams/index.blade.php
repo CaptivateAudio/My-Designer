7985469cb34427a4d39cfd053e354dcf87128778
Lester Joson <lester@podcastwebsites.com>
2018-11-09 23:22:18 +0800

    Update admin teams list view

--- mydesigner/resources/views/admin/teams/index.blade.php
+++ mydesigner/resources/views/admin/teams/index.blade.php
@@ -1,60 +1,76 @@
-@extends('layouts.app')
+@extends('layouts.admin')
+
+@section('title', 'Teams')
+
+@section('heading')
+<div id="admin-content-heading">
+    <div class="container-fluid">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <h2>Teams</h2>
+            </div>
+            <div class="col-md-6 text-right">Admin > Teams</div>
+        </div>
+    </div>
+</div>
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
+<div id="admin-content-wrap">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
 
-                <div class="row">
-                    <div class="col-md-8"><div class="panel-heading"><h1>Team</h1></div></div>
-                    <div class="col-md-4"><p class="text-right"><a href="{{ route('admin.teams.create') }}" class="btn btn-primary">Add New Team</a></p></div>
-                </div>
+                    <div class="row mb-4">
+                        <div class="col-md-12"><a href="{{ route('admin.teams.create') }}" class="btn btn-primary">+ Add New</a></div>
+                    </div>
 
-                <div class="panel-body">
+                    <div class="panel-body">
 
-                    @if(session()->has('success'))
-                        <div class="alert alert-success">
-                            {{ session('success') }}
-                        </div>
-                    @endif
-                    
-                    <table class="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Team Name</th>
-                                <th>Users</th>
-                                <th>Packages</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-
-                            @forelse($teams as $team)
-                            <tr>
-                                <td>{{ $team->id }}</td>
-                                <td>{{ $team->team_name }}</td>
-                                <td>{{ $team->users()->where('team_id', $team->id)->count() }}</td>
-                                <td>{{ $team->packages()->where('team_id', $team->id)->count() }}</td>
-                                <td>
-                                    <a href="{{ route('admin.teams.edit', $team->id) }}" class="btn btn-outline-primary">Edit</a>
-                                    <form action="{{ route('admin.teams.destroy', $team->id) }}" method="POST"
-                                          style="display: inline"
-                                          onsubmit="return confirm('Are you sure?');">
-                                        {{ method_field('DELETE') }}
-                                        {{ csrf_field() }}
-                                        <button class="btn btn-outline-danger">Delete</button>
-                                    </form>
-                                </td>
-                            </tr>
-                            @empty
+                        @if(session()->has('success'))
+                            <div class="alert alert-success">
+                                {{ session('success') }}
+                            </div>
+                        @endif
+                        
+                        <table class="table table-bordered">
+                            <thead>
                                 <tr>
-                                    <td colspan="4">No teams found.</td>
+                                    <th>ID</th>
+                                    <th>Team Name</th>
+                                    <th>Users</th>
+                                    <th>Packages</th>
+                                    <th>Actions</th>
                                 </tr>
-                            @endforelse
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+
+                                @forelse($teams as $team)
+                                <tr>
+                                    <td>{{ $team->id }}</td>
+                                    <td>{{ $team->team_name }}</td>
+                                    <td>{{ $team->users()->where('team_id', $team->id)->count() }}</td>
+                                    <td>{{ $team->packages()->where('team_id', $team->id)->count() }}</td>
+                                    <td>
+                                        <a href="{{ route('admin.teams.edit', $team->id) }}" class="btn btn-outline-primary">Edit</a>
+                                        <form action="{{ route('admin.teams.destroy', $team->id) }}" method="POST"
+                                              style="display: inline"
+                                              onsubmit="return confirm('Are you sure?');">
+                                            {{ method_field('DELETE') }}
+                                            {{ csrf_field() }}
+                                            <button class="btn btn-outline-danger">Delete</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="5">No teams found.</td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
