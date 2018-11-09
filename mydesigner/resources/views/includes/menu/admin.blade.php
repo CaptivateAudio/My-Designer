8e81fa4a267a7b3afbb197cd6ca5bc1c2a5652cf
Lester Joson <lester@podcastwebsites.com>
2018-11-09 23:14:58 +0800

    Add menu for admin role

---
+++ mydesigner/resources/views/includes/menu/admin.blade.php
@@ -0,0 +1,67 @@
+<ul class="nav nav-pills flex-column">
+    <li class="nav-item">
+        <a class="nav-link" href="{{ url('/dashboard') }}">
+            <span class="menu-icon"><i class="fas fa-tachometer-alt"></i></span>
+            <span class="menu-name">Dashboard</span>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#item-teams">
+            <span class="menu-icon"><i class="fas fa-user-friends"></i></span>
+            <span class="menu-name">Teams</span>
+        </a>
+        <div id="item-teams" class="collapse">
+            <ul class="nav flex-column ml-3">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('admin.teams.index') }}">All Teams</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('admin.teams.create') }}">Add New</a>
+                </li>
+            </ul>
+        </div>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#item-packages">
+            <span class="menu-icon"><i class="fas fa-box"></i></span>
+            <span class="menu-name">Packages</span>
+        </a>
+        <div id="item-packages" class="collapse">
+            <ul class="nav flex-column ml-3">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('admin.packages.index') }}">All Packages</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('admin.packages.create') }}">Add New</a>
+                </li>
+            </ul>
+        </div>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="#">
+            <span class="menu-icon"><i class="fas fa-paint-brush"></i></span>
+            <span class="menu-name">Designs</span>
+        </a>
+    </li>
+            
+    <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#item-users">
+            <span class="menu-icon"><i class="fas fa-users"></i></span>
+            <span class="menu-name">Users</span>
+        </a>
+        <div id="item-users" class="collapse">
+            <ul class="nav flex-column ml-3">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('admin.users.index') }}">All Users</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('admin.users.create') }}">Add New</a>
+                </li>
+            </ul>
+        </div>
+    </li>
+
+</ul>
