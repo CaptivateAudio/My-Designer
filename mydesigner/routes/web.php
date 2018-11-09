231a277707a62e3badded48297ba28386d0d2f86
Lester Joson <lester@podcastwebsites.com>
2018-11-09 23:19:52 +0800

    Add user and designer role routes

--- mydesigner/routes/web.php
+++ mydesigner/routes/web.php
@@ -29,6 +29,10 @@ Route::get('/account',  ['as' => 'account.show', 'uses' => 'AccountController@sh
 Route::get('/edit-account',  ['as' => 'account.edit', 'uses' => 'AccountController@edit']);
 Route::patch('/update-account',  ['as' => 'account.update', 'uses' => 'AccountController@update']);
 
+/* User and Designer Routes */
+Route::get('/packages',  ['as' => 'user.packages.list', 'uses' => 'DesignController@packages']);
+Route::get('/designs/request/{design_id}',  ['as' => 'user.designs.requests.view', 'uses' => 'DesignController@view_design_request']);
+
 /**
  * Admin Routes
  */
@@ -40,7 +44,7 @@ Route::namespace('Admin')
         Route::resource('users', 'UserController');
         Route::resource('teams', 'TeamController');
         Route::resource('packages', 'PackageController');
-	});
+    });
 
 Route::get('/admin/user/{id}/styles',  ['as' => 'admin.users.styles', 'uses' => 'Style\StyleController@admin_view']);
 
@@ -52,4 +56,16 @@ Route::namespace('Style')
     ->name('style.')
     ->group(function () {
         Route::resource('settings', 'StyleController');
-	});
+    });
+
+Route::get('/designs/new/{package_id}',  ['as' => 'user.request.design', 'uses' => 'DesignController@design_request']);
+Route::resource('design',  'DesignController')->only([ 'store' ]);
+Route::get('/designs/requests',  ['as' => 'user.designs.requests', 'uses' => 'DesignController@list_design_request']);
+
+/**
+ * Designer Routes
+ */
+Route::get('/packages/designs/requests/{package_id}',  ['as' => 'designer.team.designs.requests', 'uses' => 'DesignController@team_design_request']);
+Route::put('/design/request/assign/{design_id}',  ['as' => 'designer.assign.designs.request', 'uses' => 'DesignController@assign_design_request']);
+
+//Route::get('/designs/requests/request',  ['as' => 'designer.team.designs.requests', 'uses' => 'DesignController@team_design_request']);
