a8e616be383c6fac8db5227f0ee68f3f151299cb
Lester Joson <lester@podcastwebsites.com>
2018-11-09 23:22:25 +0800

    Update admin teams edit view

--- mydesigner/resources/views/admin/teams/edit.blade.php
+++ mydesigner/resources/views/admin/teams/edit.blade.php
@@ -1,50 +1,65 @@
-@extends('layouts.app')
+@extends('layouts.admin')
+
+@section('title', 'Teams')
+
+@section('heading')
+<div id="admin-content-heading">
+    <div class="container-fluid">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <h2>Edit Team</h2>
+            </div>
+            <div class="col-md-6 text-right">Admin > Teams > Edit</div>
+        </div>
+    </div>
+</div>
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading"><h1>Edit Team</h1></div>
-
-                @if(session()->has('success'))
-                    <div class="alert alert-success">
-                        {{ session('success') }}
-                    </div>
-                @endif
-
-                @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
-
-                <div class="panel-body">
-                    <form action="{{ route('admin.teams.update', $team->id) }}" method="post">
-                        <input type="hidden" name="_method" value="PUT">
-                        {{ csrf_field() }}
-
-                        <div class="form-group row">
-                            <label class="col-sm-4 col-form-label">Team Name</label>
-                            <div class="col-sm-8">
-                                <input type="text" name="team_name" class="form-control" value="{{ $team->team_name }}" />
-                            </div>
+<div id="admin-content-wrap">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    @if(session()->has('success'))
+                        <div class="alert alert-success">
+                            {{ session('success') }}
                         </div>
+                    @endif
 
-                        <div class="form-group row">
-                            <label class="col-sm-4 col-form-label"></label>
-                            <div class="col-sm-2">
-                                <button class="btn btn-primary btn-block" type="submit">Save Changes</button>
-                            </div>
-                            <div class="col-sm-2">
-                                <a class="btn btn-secondary" href="{{ route('admin.teams.index') }}">Cancel</a>
-                            </div>
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
                         </div>
-                    </form>
+                    @endif
+
+                    <div class="panel-body">
+                        <form action="{{ route('admin.teams.update', $team->id) }}" method="post">
+                            <input type="hidden" name="_method" value="PUT">
+                            {{ csrf_field() }}
+
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">Team Name</label>
+                                <div class="col-sm-8">
+                                    <input type="text" name="team_name" class="form-control" value="{{ $team->team_name }}" />
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label"></label>
+                                <div class="col-sm-2">
+                                    <button class="btn btn-primary btn-block" type="submit">Save Changes</button>
+                                </div>
+                                <div class="col-sm-2">
+                                    <a class="btn btn-secondary" href="{{ route('admin.teams.index') }}">Cancel</a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
