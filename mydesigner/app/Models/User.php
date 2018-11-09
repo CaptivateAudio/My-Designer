d341ebb71c9bcfef6a2822f7d8c482744a74d008
Lester Joson <lester@podcastwebsites.com>
2018-11-09 23:25:36 +0800

    Add user designs relation and user feedback relationship

--- mydesigner/app/Models/User.php
+++ mydesigner/app/Models/User.php
@@ -42,6 +42,16 @@ class User extends Authenticatable
 	{
 	  return $this->belongsToMany(Style::class);
 	}
+
+	public function designs()
+	{
+	  return $this->belongsToMany(Design::class);
+	}
+
+	public function feedback()
+    {
+        return $this->hasMany(Feedback::class);
+    }
 	
 	/**
 	* @param string|array $roles
