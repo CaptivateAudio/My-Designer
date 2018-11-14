2e716b25a5b7e6c74d406a0f0ef789f31b710878
Lester Joson <lester@podcastwebsites.com>
2018-11-14 17:20:59 +0800

    Update routes - define feedback resource - admin design view requests - admin design request view - design request approve function (user) - feedback submit post function - feedback delete function (manager) - feedback update function (manager) - user style view

--- mydesigner/routes/web.php
+++ mydesigner/routes/web.php
@@ -33,6 +33,8 @@ Route::patch('/update-account',  ['as' => 'account.update', 'uses' => 'AccountCo
 Route::get('/packages',  ['as' => 'user.packages.list', 'uses' => 'DesignController@packages']);
 Route::get('/designs/request/{design_id}',  ['as' => 'user.designs.requests.view', 'uses' => 'DesignController@view_design_request']);
 
+Route::resource('feedback',  'FeedbackController')->only([ 'store' ]);
+
 /**
  * Admin Routes
  */
@@ -47,6 +49,9 @@ Route::namespace('Admin')
     });
 
 Route::get('/admin/user/{id}/styles',  ['as' => 'admin.users.styles', 'uses' => 'Style\StyleController@admin_view']);
+Route::get('/admin/designs/requests',  ['as' => 'admin.designs.requests', 'uses' => 'DesignController@admin_list_design_request']);
+//Route::get('/admin/designs/requests/{status}',  ['as' => 'admin.designs.requests', 'uses' => 'DesignController@admin_list_design_request']);
+Route::get('/admin/designs/request/{design_id}',  ['as' => 'admin.designs.requests.view', 'uses' => 'DesignController@admin_view_design_request']);
 
 /**
  * User Routes
@@ -61,6 +66,7 @@ Route::namespace('Style')
 Route::get('/designs/new/{package_id}',  ['as' => 'user.request.design', 'uses' => 'DesignController@design_request']);
 Route::resource('design',  'DesignController')->only([ 'store' ]);
 Route::get('/designs/requests',  ['as' => 'user.designs.requests', 'uses' => 'DesignController@list_design_request']);
+Route::put('/design/request/{design_id}/approve/',  ['as' => 'user.design.request.approve', 'uses' => 'DesignController@approve_design_request']);
 
 /**
  * Designer Routes
@@ -69,3 +75,15 @@ Route::get('/packages/designs/requests/{package_id}',  ['as' => 'designer.team.d
 Route::put('/design/request/assign/{design_id}',  ['as' => 'designer.assign.designs.request', 'uses' => 'DesignController@assign_design_request']);
 
 //Route::get('/designs/requests/request',  ['as' => 'designer.team.designs.requests', 'uses' => 'DesignController@team_design_request']);
+
+
+Route::post('/design/{design_id}/feedback/submit', ['as' => 'user.design.feedback.submit', 'uses' => 'FeedbackController@feedback_submit']);
+
+Route::get('/user/view/{user_id}',  ['as' => 'user.view', 'uses' => 'Style\StyleController@user_view']);
+
+/**
+ * Manager Routes
+ */
+Route::put('/feedback/approve/{design_id}/{feedback_id}',  ['as' => 'manager.feedback.approve', 'uses' => 'FeedbackController@approve_feedback']);
+Route::delete('/feedback/delete/{design_id}/{feedback_id}',  ['as' => 'manager.feedback.delete', 'uses' => 'FeedbackController@delete_feedback']);
+Route::patch('/feedback/update/{design_id}/{feedback_id}',  ['as' => 'manager.feedback.update', 'uses' => 'FeedbackController@update_feedback']);
