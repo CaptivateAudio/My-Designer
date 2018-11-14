3e2c5c442117c0f9ce839fbb770f8f04bde5e588
Lester Joson <lester@podcastwebsites.com>
2018-11-14 17:23:52 +0800

    Update admin menu

--- mydesigner/resources/views/includes/menu/admin.blade.php
+++ mydesigner/resources/views/includes/menu/admin.blade.php
@@ -6,13 +6,13 @@
         </a>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item has-submenu">
         <a class="nav-link" data-toggle="collapse" href="#item-teams">
             <span class="menu-icon"><i class="fas fa-user-friends"></i></span>
             <span class="menu-name">Teams</span>
         </a>
         <div id="item-teams" class="collapse">
-            <ul class="nav flex-column ml-3">
+            <ul class="nav submenu flex-column ml-3">
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('admin.teams.index') }}">All Teams</a>
                 </li>
@@ -23,13 +23,13 @@
         </div>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item has-submenu">
         <a class="nav-link" data-toggle="collapse" href="#item-packages">
             <span class="menu-icon"><i class="fas fa-box"></i></span>
             <span class="menu-name">Packages</span>
         </a>
         <div id="item-packages" class="collapse">
-            <ul class="nav flex-column ml-3">
+            <ul class="nav submenu flex-column ml-3">
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('admin.packages.index') }}">All Packages</a>
                 </li>
@@ -41,19 +41,19 @@
     </li>
 
     <li class="nav-item">
-        <a class="nav-link" href="#">
+        <a class="nav-link" href="{{ route('admin.designs.requests') }}">
             <span class="menu-icon"><i class="fas fa-paint-brush"></i></span>
             <span class="menu-name">Designs</span>
         </a>
     </li>
             
-    <li class="nav-item">
+    <li class="nav-item has-submenu">
         <a class="nav-link" data-toggle="collapse" href="#item-users">
             <span class="menu-icon"><i class="fas fa-users"></i></span>
             <span class="menu-name">Users</span>
         </a>
         <div id="item-users" class="collapse">
-            <ul class="nav flex-column ml-3">
+            <ul class="nav submenu flex-column ml-3">
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('admin.users.index') }}">All Users</a>
                 </li>
