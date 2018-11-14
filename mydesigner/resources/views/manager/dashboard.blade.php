20cfbe81382850058d5e62da9c71c1d6681544b6
Lester Joson <lester@podcastwebsites.com>
2018-11-14 17:12:18 +0800

    Add manager dashboard view

---
+++ mydesigner/resources/views/manager/dashboard.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.admin')
+
+@section('title', 'Dashboard')
+
+@section('heading')
+<div id="admin-content-heading">
+    <div class="container-fluid">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <h2>Dashboard</h2>
+            </div>
+            <div class="col-md-6"></div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('content')
+<div id="admin-content-wrap">
+    <div class="container-fluid">
+
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+                <div class="card">
+                    <div class="card-header"><strong>Quick Links</strong></div>
+
+                    <div class="card-body">
+
+                        <div class="row align-items-center">
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="col-md-2">
+                                        <a class="btn btn-primary btn-block" href="{{ url('/') }}"><i class="fas fa-home"></i> HOME</a>
+                                    </div>
+                                    <div class="col-md-2">
+                                        <a class="btn btn-primary btn-block" href="{{ route('account.show') }}"><i class="fas fa-user"></i> ACCOUNT</a>
+                                    </div>
+                                    <div class="col-md-2">
+                                        <a class="btn btn-primary btn-block" href="{{ route('user.designs.requests') }}"><i class="fas fa-paint-brush"></i> DESIGNS</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="row mb-4 mt-4">
+            <div class="col-md-4">
+
+                <div class="card">
+                    <div class="card-header"><strong>Designs</strong></div>
+
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</div>
+@endsection
