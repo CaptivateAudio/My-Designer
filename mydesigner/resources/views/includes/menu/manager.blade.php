b4b83e749eb5f4219dbc16047c84ba5a6bf56aa6
Lester Joson <lester@podcastwebsites.com>
2018-11-14 17:23:39 +0800

    Update manager menu

--- mydesigner/resources/views/includes/menu/manager.blade.php
+++ mydesigner/resources/views/includes/menu/manager.blade.php
@@ -6,45 +6,18 @@
         </a>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item has-submenu">
         <a class="nav-link" data-toggle="collapse" href="#item-teams">
-            <span class="menu-icon"><i class="fas fa-user-friends"></i></span>
-            <span class="menu-name">Teams</span>
-        </a>
-        <div id="item-teams" class="collapse">
-            <ul class="nav flex-column ml-3">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('admin.teams.index') }}">All Teams</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('admin.teams.create') }}">Add New</a>
-                </li>
-            </ul>
-        </div>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link" data-toggle="collapse" href="#item-packages">
-            <span class="menu-icon"><i class="fas fa-box"></i></span>
-            <span class="menu-name">Packages</span>
-        </a>
-        <div id="item-packages" class="collapse">
-            <ul class="nav flex-column ml-3">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('admin.packages.index') }}">All Packages</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('admin.packages.create') }}">Add New</a>
-                </li>
-            </ul>
-        </div>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link" href="#">
             <span class="menu-icon"><i class="fas fa-paint-brush"></i></span>
             <span class="menu-name">Designs</span>
         </a>
+        <div id="item-teams" class="collapse">
+            <ul class="nav submenu flex-column ml-3">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('user.designs.requests') }}">All Designs</a>
+                </li>
+            </ul>
+        </div>
     </li>
 
 </ul>
