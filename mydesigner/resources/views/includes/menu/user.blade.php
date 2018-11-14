9e9431fe67e2c0f0e0fe4eb1be889b45135d1302
Lester Joson <lester@podcastwebsites.com>
2018-11-14 17:23:27 +0800

    Update user menu

--- mydesigner/resources/views/includes/menu/user.blade.php
+++ mydesigner/resources/views/includes/menu/user.blade.php
@@ -13,18 +13,25 @@
         </a>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item has-submenu">
         <a class="nav-link" data-toggle="collapse" href="#item-teams">
             <span class="menu-icon"><i class="fas fa-paint-brush"></i></span>
             <span class="menu-name">Designs</span>
         </a>
         <div id="item-teams" class="collapse">
-            <ul class="nav flex-column ml-3">
+            <ul class="nav submenu flex-column ml-3">
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('user.designs.requests') }}">All Designs</a>
                 </li>
             </ul>
         </div>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('style.settings.index') }}">
+            <span class="menu-icon"><i class="fas fa-palette"></i></span>
+            <span class="menu-name">Styles</span>
+        </a>
     </li>
 
 </ul>
