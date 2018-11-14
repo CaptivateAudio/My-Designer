2056f3bc55dde5b96148f3452d83fd4885ac1b09
Lester Joson <lester@podcastwebsites.com>
2018-11-14 17:12:02 +0800

    Add manager design requests list view

---
+++ mydesigner/resources/views/manager/designs/requests.blade.php
@@ -0,0 +1,126 @@
+@extends('layouts.admin')
+
+@section('title', 'Design Requests')
+
+@section('heading')
+<div id="admin-content-heading">
+    <div class="container-fluid">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <h2>Design Requests</h2>
+            </div>
+            <div class="col-md-6 text-right">User > Design Requests</div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('content')
+<div id="admin-content-wrap">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+
+                    @if(session()->has('success'))
+                        <div class="alert alert-success">
+                            {{ session('success') }}
+                        </div>
+                    @endif
+
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    
+                    <div class="panel-body">
+                        <div class="row">
+                            @foreach($designs as $design)
+                                <div class="col-md-4 mt-4 mb-4">
+                                    <div class="card">
+                                        <div class="card-header"><strong>{{ $design->id }} | {{ $design->package_name }}</strong>
+                                            <div class="h2 float-right">
+                                                @switch($design->status)
+                                                    @case('request')
+                                                        <span class="badge badge-secondary">{{ $design->status }}</span>
+                                                    @break
+
+                                                    @case('in-progress')
+                                                        <span class="badge badge-info">{{ $design->status }}</span>
+                                                    @break
+
+                                                    @case('approved')
+                                                        <span class="badge badge-success">{{ $design->status }}</span>
+                                                    @break
+
+                                                    @case('rejected')
+                                                        <span class="badge badge-dark">{{ $design->status }}</span>
+                                                    @break
+
+                                                    @case('cancelled')
+                                                        <span class="badge badge-light">{{ $design->status }}</span>
+                                                    @break
+
+                                                    @default
+                                                        <span class="badge badge-warning">{{ $design->status }}</span>
+                                                    @break
+                                                @endswitch
+                                            </div>
+                                        </div>
+
+                                        <div class="card-body">
+                                            @php
+                                            $user = $design->users()->wherePivot('type', 'user');
+                                            @endphp
+                                            @if( $user->count() )
+                                                @php
+                                                    $user_account = $user->first()
+                                                @endphp
+                                                
+                                                <p>Requested By: {{ $user_account->first_name }} {{ $user_account->last_name }}</p>
+                                            @endif
+                                            
+                                            @php
+                                            $designer = $design->users()->wherePivot('type', 'designer');
+                                            @endphp
+                                            @if( $designer->count() )
+                                                @php
+                                                    $designer_account = $designer->first()
+                                                @endphp
+                                                
+                                                <p>Designer: {{ $designer_account->first_name }} {{ $designer_account->last_name }}</p>
+                                            @endif
+
+                                            @if( !empty( $design->details ) )
+                                            <p>@php echo nl2br($design->details) @endphp</p>
+                                            @endif
+
+                                            @if( !empty( $design->completion_date ) )
+                                            <p>Completion Date: {{ $design->completion_date }}</p>
+                                            @endif
+                                            <p class="text-center">
+                                                <a href="{{ route('user.designs.requests.view', $design->id) }}"><button class="btn btn-outline-secondary">View Design Request</button></a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>      
+                            @endforeach
+
+                            @if($designs->count() == 0)
+                                <div class="col-md-12">
+                                    No results found.
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
