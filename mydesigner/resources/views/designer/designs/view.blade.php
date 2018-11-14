5cdf6fdc413542d6a6f4f2132f7a652b9b846c7e
Lester Joson <lester@podcastwebsites.com>
2018-11-14 17:24:18 +0800

    Update designer designs view

--- mydesigner/resources/views/designer/designs/view.blade.php
+++ mydesigner/resources/views/designer/designs/view.blade.php
@@ -18,6 +18,7 @@
 @section('content')
 <div id="admin-content-wrap">
     <div class="container-fluid">
+        
         <div class="row">
             <div class="col-md-12">
                 <div class="panel panel-default">
@@ -40,36 +41,35 @@
                     <div class="panel-body">
                         <div class="row">
                             <div class="col-md-12">
-
-                                <strong>{{ $design->id }}</strong>
-                                <div class="h2 float-right">
+                                
+                                <div class="h3">
                                     @switch($design->status)
                                         @case('request')
-                                            <span class="badge badge-secondary">{{ $design->status }}</span>
+                                            <span class="badge badge-secondary text-white"><strong>ID# : {{ $design->id }}</strong> | {{ $design->status }}</span>
                                         @break
 
                                         @case('in-progress')
-                                            <span class="badge badge-info">{{ $design->status }}</span>
+                                            <span class="badge badge-info text-white"><strong>ID# : {{ $design->id }}</strong> | {{ $design->status }}</span>
                                         @break
 
                                         @case('approved')
-                                            <span class="badge badge-success">{{ $design->status }}</span>
+                                            <span class="badge badge-success text-white"><strong>ID# : {{ $design->id }}</strong> | {{ $design->status }}</span>
                                         @break
 
                                         @case('rejected')
-                                            <span class="badge badge-dark">{{ $design->status }}</span>
+                                            <span class="badge badge-dark text-white"><strong>ID# : {{ $design->id }}</strong> | {{ $design->status }}</span>
                                         @break
 
                                         @case('cancelled')
-                                            <span class="badge badge-light">{{ $design->status }}</span>
+                                            <span class="badge badge-light text-white"><strong>ID# : {{ $design->id }}</strong> | {{ $design->status }}</span>
                                         @break
 
                                         @default
-                                            <span class="badge badge-warning">{{ $design->status }}</span>
+                                            <span class="badge badge-warning text-white"><strong>ID# : {{ $design->id }}</strong> | {{ $design->status }}</span>
                                         @break
                                     @endswitch
                                 </div>
-                                <br>
+
                                 @php
                                 $user = $design->users()->wherePivot('type', 'user');
                                 @endphp
@@ -106,6 +106,122 @@
                             </div>      
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row mt-4 mb-4">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <form method="POST" action="{{ route('user.design.feedback.submit', $design->id) }}" enctype="multipart/form-data">
+                        {{ csrf_field() }}
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Message</label>
+                            <div class="col-sm-8">
+                                <textarea name="comment" rows="4" class="form-control"></textarea>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Version</label>
+                            <div class="col-sm-8">
+                                <input type="text" name="version" class="form-control" value="" />
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Attachment</label>
+                            <div class="col-sm-8">
+                                <input type="file" name="attachment" class="form-control" value="" />
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label"></label>
+                            <div class="col-sm-8">
+                                <button class="btn btn-primary" type="submit">Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div class="feedback-wrap">
+            <div class="row">
+                <div class="col-md-12">
+                    @forelse( $feedback_lists as $feedback )
+                        @php
+                        $role_name = $feedback->user->roles->first()['role_name'];
+
+                        $commented_by = $feedback->user()->first();
+                        @endphp
+                        <div class="feedback {{ $role_name }}-feedback">
+                            @if( $feedback->status == 'pending')
+                                @php $status_class = 'row row-eq-height border border-warning'; @endphp
+                            @else
+                                @php $status_class = 'row row-eq-height'; @endphp
+                            @endif
+                            <div class="{{ $status_class }}">
+                                <div class="col-md-3 feedback-sender-wrap">
+                                    <div class="feedback-sender">
+                                        <div class="avatar">
+                                            @php
+                                                if( ! empty ( $feedback->user()->first()['avatar'] ) ) :
+                                                    $avatar_url = URL::to('/').'/uploads/'.$commented_by['id'].'/avatar/'. $commented_by->attachments->first()['filename'];
+                                                else :
+                                                    $avatar_url = asset('images/user.jpg');
+                                                endif;
+                                            @endphp
+
+                                            @if( $role_name == 'user' )
+                                                <a href="{{ route('user.view', $commented_by['id']) }}"><img src="{{ $avatar_url }}" class="rounded-circle" width="65" height="65"></a>
+                                            @else
+                                                <img src="{{ $avatar_url }}" class="rounded-circle" width="65" height="65">
+                                            @endif
+                                        </div>
+                                        @if( $role_name == 'user' )
+                                            <div class="name"><p><strong><a href="{{ route('user.view', $commented_by['id']) }}">{{ $commented_by['first_name'] }} {{$commented_by['last_name']}}</a></strong></p></div>
+                                        @else
+                                            <div class="name"><p><strong>{{ $feedback->user()->first()['first_name'] }} {{$feedback->user()->first()['last_name']}}</strong></p></div>
+                                        @endif
+                                        <p>{{ $role_name }}</p>
+                                        <p><small>{{ $feedback->updated_at }}</small></p>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-9 feedback-content-wrap">
+                                    <div class="feedback-content">
+
+                                        @if( $feedback->status == 'pending')
+                                            <p><em>Pending approval</em></p>
+                                        @endif
+
+                                        @unless( empty( $feedback->version ) )
+                                        <p><strong>Version {{ $feedback->version }}</strong></p>
+                                        @endunless
+
+                                        <p>@php echo nl2br($feedback->comment) @endphp</p>
+
+                                        @php
+                                            $attachments = array();
+                                            $attachment_count = $feedback->attachments->count();
+                                            if( $attachment_count >= 1 ){
+                                                $attachments = $feedback->attachments()->get();
+                                            }
+                                        @endphp
+
+                                        @forelse( $attachments as $attachment )
+                                            <p><a href="{{ URL::to('/') }}/uploads/{{ $commented_by['id'] }}/feedback-attachment/{{ $feedback->attachments->first()['filename'] }}" target="_blank">{{ $attachment['filename_original'] }}</a></p>
+                                        @empty
+                                        @endforelse
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @empty
+                    @endforelse
                 </div>
             </div>
         </div>
