f703aaecc670c1045e781f03966b4dd05401e299
Lester Joson <lester@podcastwebsites.com>
2018-11-14 17:26:58 +0800

    Update admin dashboard view

--- mydesigner/resources/views/admin/dashboard.blade.php
+++ mydesigner/resources/views/admin/dashboard.blade.php
@@ -29,22 +29,19 @@
                 @endif
 
                 <div class="card">
-                    <div class="card-header"><strong>Welcome to your Dashboard!</strong></div>
+                    <div class="card-header"><strong>Quick Links:</strong></div>
 
                     <div class="card-body">
 
                         <div class="row align-items-center">
-                            <div class="col-md-2"><img src="https://placeholdit.co//i/150x150?&bg=cccccc&fc=999999&text=PHOTO"></div>
-                            <div class="col-md-10">
-
-                                <h5>Quick Links:</h5>
+                            <div class="col-md-12">
 
                                 <div class="row">
                                     <div class="col-md-2">
                                         <a class="btn btn-primary btn-block" href="{{ url('/') }}"><i class="fas fa-home"></i> HOME</a>
                                     </div>
                                     <div class="col-md-2">
-                                        <a class="btn btn-primary btn-block" href="{{ url('/account') }}"><i class="fas fa-user"></i> ACCOUNT</a>
+                                        <a class="btn btn-primary btn-block" href="{{ route('account.show') }}"><i class="fas fa-user"></i> ACCOUNT</a>
                                     </div>
                                     <div class="col-md-2">
                                         <a class="btn btn-primary btn-block" href="{{ route('admin.users.index') }}"><i class="fas fa-users"></i> USERS</a>
@@ -55,11 +52,7 @@
                                     </div>
 
                                     <div class="col-md-2">
-                                        <a class="btn btn-primary btn-block" href="{{ url('/designs') }}"><i class="fas fa-paint-brush"></i> DESIGNS</a>
-                                    </div>
-
-                                    <div class="col-md-2">
-                                        <a class="btn btn-primary btn-block" href="{{ url('/history') }}"><i class="fas fa-clock"></i> HISTORY</a>
+                                        <a class="btn btn-primary btn-block" href="{{ route('admin.designs.requests') }}"><i class="fas fa-paint-brush"></i> DESIGNS</a>
                                     </div>
                                 </div>
                             </div>
@@ -134,8 +127,7 @@
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 
                         <div class="row mt-4">
-                            <div class="col-md-6"><a href="{{ url('/') }}" class="btn btn-outline-secondary btn-sm btn-block"> <i class="fas fa-plus"></i> Add New</a></div>
-                            <div class="col-md-6"><a href="{{ url('/designs') }}" class="btn btn-outline-secondary btn-sm btn-block">View All</a></div>
+                            <div class="col-md-6"><a href="{{ route('admin.designs.requests') }}" class="btn btn-outline-secondary btn-sm btn-block">View All</a></div>
                         </div>
                     </div>
                 </div>
