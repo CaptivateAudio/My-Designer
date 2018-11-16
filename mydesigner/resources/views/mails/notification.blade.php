b980070284305b8cd34d3187bffd34ea3949799c
Lester Joson <lester@podcastwebsites.com>
2018-11-16 22:55:02 +0800

    Update test notification view

--- mydesigner/resources/views/mails/notification.blade.php
+++ mydesigner/resources/views/mails/notification.blade.php
@@ -1,5 +1,20 @@
-<p>Name: {{ $name }}</p>
-<p>E-Mail: {{ $email }}</p>
-<p>Subject: {{ $title }}</p>
-<p>Message: <br>
-{{ $content }}</p>
+Hello <i>{{ $notification->receiver }}</i>,
+<p>This is a demo email for testing purposes! Also, it's the HTML version.</p>
+ 
+<p><u>Demo object values:</u></p>
+ 
+<div>
+<p><b>Demo One:</b>&nbsp;{{ $notification->demo_one }}</p>
+<p><b>Demo Two:</b>&nbsp;{{ $notification->demo_two }}</p>
+</div>
+ 
+<p><u>Values passed by With method:</u></p>
+ 
+<div>
+<p><b>testVarOne:</b>&nbsp;{{ $testVarOne }}</p>
+<p><b>testVarTwo:</b>&nbsp;{{ $testVarTwo }}</p>
+</div>
+ 
+Thank You,
+<br/>
+<i>{{ $notification->sender }}</i>
