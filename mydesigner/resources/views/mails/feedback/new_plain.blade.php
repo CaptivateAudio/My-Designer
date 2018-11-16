f3754e3cbd447d85929ea4bfbce06b67d8a61bd5
Lester Joson <lester@podcastwebsites.com>
2018-11-16 22:45:02 +0800

    Add feedback notification view - new feedback - approved feedback

---
+++ mydesigner/resources/views/mails/feedback/new_plain.blade.php
@@ -0,0 +1,7 @@
+{{ $site_title }}
+
+{{ $emailContent['heading'] }}
+
+{{ $emailContent['button_text'] }}: {{ $emailContent['button_url'] }}"
+
+&copy; {{ now()->year }} {{ $site_title }}. All rights reserved.
